1816e7af806de2b0f48fea1e19982e8037ca0198
hori0948 <nrh0028@gmail.com>
2025-07-22 09:36:23 +0900

    イベント発火 基礎編
    index.html
    style.css
    script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-let lists = document.getElementsByClassName("pull-down-list");
-console.log(lists); 
+window.addEventListener('load', function () {
+    const pullDownButton = document.getElementById("lists");
+    //const pullDownMenu = document.getElementById("pull-down");
+
+    //マウスオーバーした時
+    pullDownButton.addEventListener("mouseover", function () {
+        // pullDownMenu.classList.remove("hidden");
+        console.log("乗る");
+    });
+
+    //マウスアウトした時
+    pullDownButton.addEventListener("mouseout", function () {
+        // pullDownMenu.classList.add("hedden");
+        console.log("外れる");
+    });
+
+    //クリックした時
+    pullDownButton.addEventListener("click", function () {
+        // pullDownMenu.classList.toggle("hidden");
+        console.log("クリック");
+    });
+});
 
